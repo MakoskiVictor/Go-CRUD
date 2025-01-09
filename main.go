d31dcb484e0eea672b15572b518a7e6f5b38b450
MakoskiVictor <makoski.ed@gmail.com>
2025-01-09 17:38:20 -0300

    simple route

--- main.go
+++ main.go
@@ -3,16 +3,15 @@ package main
 import (
 	"net/http"
 
+	"github.com/MakoskiVictor/Go-CRUD/routes"
 	"github.com/gorilla/mux"
 )
 
 func main() {
-	r := mux.NewRouter() // Crea un nuevo router
+	r := mux.NewRouter() // Crea un nuevo router con gorilla/mux
 
 	// Crea una ruta que responde a la raíz del servidor
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World!"))
-	})
+	r.HandleFunc("/", routes.HomeHandler)
 
 	http.ListenAndServe(":3000", r) // Inicia el server en el puerto y utiliza r (el router creado)
 }
